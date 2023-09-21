6e580787ae9356db82807578365120930a10aee5
Code_Developer23 <dev23@finoservices.com>
2023-09-21 13:52:01 -0500

    Se crea el archivo recap.ts para repasar conceptos Se modifica app.component.htm

---
+++ src/app/recap.ts
@@ -0,0 +1,14 @@
+const username: string | number = 'polaco91';
+
+const sum = (num1: number, num2: number) => {
+  return num1 + num2;
+}
+sum(25, 3)
+
+class Person{
+
+  constructor(public age: number, public lastname: string){}
+}
+
+const polaco = new Person(32, 'Tobon');
+
