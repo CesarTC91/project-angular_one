0d922e027ad4f5b3fecc372ff86cac558bb3802b
Code_Developer23 <dev23@finoservices.com>
2023-09-22 10:53:37 -0500

    Se estudia la funcionalidad de los eventos Se crea unos estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'project-angular_one';
+  name = 'Cesar'
+  age = 32
+  image = 'https://www.motriz.info/wp-content/uploads/2015/06/Corolla-VS-Fluence-DSC_4550-1040x520.jpg'
+
+  btnDisabled = true
+
+  person = {
+    name: 'Astrid',
+    age: 29
+  }
+
+  toggleButton(){
+    this.btnDisabled = !this.btnDisabled
+  }
+
+  increaseAge(){
+    this.person.age += 1
+  }
+
+  decreaseAge(){
+    this.person.age -= 1
+  }
+
+  onScroll(event: Event){
+    const element = event.target as HTMLElement
+    console.log(element.scrollTop)
+  }
+
 }
